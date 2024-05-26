797af2080a9af9afbf2e611d4df3357e931f6af9
alexgaarciia <al.garcia636@gmail.com>
2024-05-26 12:10:01 +0200

    original file of DQN (neural_networks_q_learning.py) and modified version to use random forests (random_forests_q_learning.py)

--- random_forests_q_learning.py
+++ random_forests_q_learning.py
@@ -1,8 +1,6 @@
 import gymnasium as gym
 import math
 import random
-import matplotlib
-import matplotlib.pyplot as plt
 from collections import namedtuple, deque
 from itertools import count
 
@@ -10,26 +8,15 @@ import torch
 from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 
-env = gym.make("CartPole-v1")
-
-# set up matplotlib
-is_ipython = 'inline' in matplotlib.get_backend()
-if is_ipython:
-    from IPython import display
-
-plt.ion()
+env = gym.make("CartPole-v1", render_mode="human")
 
 # if GPU is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 # REPLAY MEMORY
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
-
-class ReplayMemory(object):
-
+class ReplayMemory:
     def __init__(self, capacity):
         self.memory = deque([], maxlen=capacity)
 
@@ -43,8 +30,7 @@ class ReplayMemory(object):
     def __len__(self):
         return len(self.memory)
 
-
-# RF ALGORITHM:
+# Random Forest Q-Learning Model
 class RandomForestQ:
     def __init__(self, n_estimators=10):
         self.model = RandomForestRegressor(n_estimators=n_estimators)
@@ -60,14 +46,12 @@ class RandomForestQ:
         else:
             return np.zeros((X.shape[0], 2))
 
-
-# TRAINING
+# TRAINING PARAMETERS
 BATCH_SIZE = 128
 GAMMA = 0.99
 EPS_START = 0.9
 EPS_END = 0.05
 EPS_DECAY = 1000
-TAU = 0.005
 
 n_actions = env.action_space.n
 state, info = env.reset()
@@ -79,12 +63,10 @@ target_net = RandomForestQ()
 memory = ReplayMemory(10000)
 steps_done = 0
 
-
 def select_action(state):
     global steps_done
     sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-                    math.exp(-1. * steps_done / EPS_DECAY)
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
@@ -94,46 +76,14 @@ def select_action(state):
     else:
         return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
 
-
-episode_durations = []
-
-
-def plot_durations(show_result=False):
-    plt.figure(1)
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
-    if show_result:
-        plt.title('Result')
-    else:
-        plt.clf()
-        plt.title('Training...')
-    plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
-    if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-        means = torch.cat((torch.zeros(99), means))
-        plt.plot(means.numpy())
-
-    plt.pause(0.001)
-    if is_ipython:
-        if not show_result:
-            display.display(plt.gcf())
-            display.clear_output(wait=True)
-        else:
-            display.display(plt.gcf())
-
-
-# TRAINING LOOP
 def optimize_model():
     if len(memory) < BATCH_SIZE:
         return
     transitions = memory.sample(BATCH_SIZE)
     batch = Transition(*zip(*transitions))
 
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                            batch.next_state)), device=device, dtype=torch.bool)
-    non_final_next_states = torch.cat([s for s in batch.next_state
-                                       if s is not None])
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
     state_batch = torch.cat(batch.state)
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward)
@@ -151,7 +101,7 @@ def optimize_model():
 
     policy_net.fit(X, y)
 
-# main training loop
+# Main training loop
 if torch.cuda.is_available():
     num_episodes = 600
 else:
@@ -179,12 +129,11 @@ for i_episode in range(num_episodes):
 
         target_net.model = policy_net.model
 
+        # Render the environment to visualize the agent
+        env.render()
+
         if done:
-            episode_durations.append(t + 1)
-            plot_durations()
             break
 
 print('Complete')
-plot_durations(show_result=True)
-plt.ioff()
-plt.show()
+env.close()
